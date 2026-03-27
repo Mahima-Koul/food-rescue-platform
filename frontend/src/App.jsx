726825dc44189f9e6465fb1eb830c47e1c193339
Maanya Chawla <maanya@combinehealth.ai>
2026-03-27 23:32:36 +0530

    added ngo dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import RestaurantDashboard from "./pages/RestaurantDashboard";
+import NGODashboard from "./pages/NGODashboard";
 import Home from "./pages/Home";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/restaurant-dashboard" element={<RestaurantDashboard />} />
+        <Route path="/ngo-dashboard" element={<NGODashboard />} />
       </Routes>
     </Router>
   );
