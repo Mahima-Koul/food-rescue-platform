c2b20443f9ea600ae0c0765724178921ac3013d0
vishrutha-03 <vishrutha.m06@gmail.com>
2026-03-27 20:46:05 +0530

    auth login signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState } from "react";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
 function App() {
-  const [showSignup, setShowSignup] = useState(false);
-
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Signup />} />
-
+        <Route path="/" element={<Login />} />   {/* ✅ FIXED */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,26 +1,39 @@
 import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleLogin = async (e) => {
+  e.preventDefault();
 
-    if (!email || !password) {
-      setError("All fields are required");
-      return;
-    }
-
-    if (!email.includes("@")) {
-      setError("Enter a valid email");
-      return;
-    }
-
+  try {
     setError("");
-    alert("Login successful");
-  };
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
+    const user = userCredential.user;
+
+    const token = await user.getIdToken();
+
+    console.log("Login success", token);
+
+    alert("Login successful 🚀");
+
+    // 👉 redirect (important)
+    window.location.href = "/dashboard";
+
+  }catch (error) {
+  console.log(error);
+  setError(error.message);
+}
+};
 
   return (
     <div className="h-screen flex justify-center items-center bg-gray-100">
@@ -28,7 +41,7 @@ function Login() {
         
         <h2 className="mb-5 text-green-600 text-2xl font-bold">Login</h2>
 
-        <form onSubmit={handleSubmit} noValidate>
+        <form onSubmit={handleLogin} noValidate>
           <input
             type="email"
             placeholder="Enter email"
